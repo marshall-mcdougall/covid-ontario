cf5bbc9d0d5c4cac128f7af9b693d5fb9cab26f0
Marshall.McDougall <marshall.mcdougall@gmail.com>
2021-02-22 21:00:53 -0500

    updating data for Feb 22nd

--- covid-python.bat
+++ covid-python.bat
@@ -1,2 +1,5 @@
-"C:\Projects\Python\python.exe" "C:\Users\Marshall.McDougall\Documents\covid\covid-ontario\ontario-covid-api.py"
+call "C:\Projects\Python\Scripts\activate.bat" 
+
+call python "C:\Users\Marshall.McDougall\Documents\covid\covid-ontario\ontario-covid-api.py"
+
 pause
